20302a4d01cf4a99ebe0fb3e5e81d930d6a257ac
amitiwary999 <tiwarya9@gmail.com>
2018-01-06 20:19:00 +0530

    find number at given position of binary version

---
+++ binarypos.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class BinaryPos{
+
+ public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter number of test cases");
+  int testcase = sc.nextInt();
+  for(int i=0;i<testcase;i++){
+   System.out.println("Enter number, number of iteration and position");
+   int num = sc.nextInt();
+   int ite = sc.nextInt();
+   int pos = sc.nextInt();
+   int mul = 1;
+   int binaryVal = 0;
+   while(num != 0){
+    int rem = num%2;
+    binaryVal = binaryVal + rem*mul;
+    mul = mul*10;
+    num = num/2;
+   }
+   String binaryval = Integer.toString(binaryVal);
+   for(int j=0;j<ite;j++){
+    String tempNum = "";
+     for(int k=0;k<binaryval.length();k++){
+       if(k>pos)
+        break;
+       if(binaryval.charAt(k) == '0'){
+        tempNum = tempNum+"01";
+     }else if(binaryval.charAt(k) == '1'){
+        tempNum = tempNum+"10";
+     }
+    }
+   binaryval = tempNum;
+   }
+  System.out.print("number at pos "+pos+" is ");
+  System.out.println(binaryval.charAt(pos));
+  }
+ }
+}
