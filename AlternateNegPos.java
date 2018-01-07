132ee291213acdceacdada04f7b4d79425bde6cc
amitiwary999 <tiwarya9@gmail.com>
2018-01-07 21:31:21 +0530

    alternate positive negative number

--- AlternateNegPos.java
+++ AlternateNegPos.java
@@ -9,6 +9,7 @@ class AlternatePosNeg{
    int arrPos[] = new int[size/2];
    int arrNeg[] = new int[size/2];
    int flagn=0,flagp=0;
+   System.out.println("Enter numbers");
    for(int i=0;i<size;i++){
     int num = sc.nextInt();
     if(num<0){
