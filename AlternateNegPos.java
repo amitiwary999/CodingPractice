32f378b03912f7387246a3963b0313146827cf7f
amitiwary999 <tiwarya9@gmail.com>
2018-01-07 21:29:31 +0530

    matrixflip

---
+++ AlternateNegPos.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class AlternatePosNeg{
+
+  public static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter number of items in array");
+   int size = sc.nextInt();
+   int arrPos[] = new int[size/2];
+   int arrNeg[] = new int[size/2];
+   int flagn=0,flagp=0;
+   for(int i=0;i<size;i++){
+    int num = sc.nextInt();
+    if(num<0){
+      arrNeg[flagn++] = num;
+    }else{
+      arrPos[flagp++] = num;
+    }
+   }
+  for(int i=0;i<size/2;i++){
+   System.out.print(arrPos[i]+" "+arrNeg[i]+" ");  
+  }
+ }
+}
