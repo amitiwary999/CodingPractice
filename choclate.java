a8f9760e7ecdfaf5ea166bc52579f31780f846f8
amitiwary999 <tiwarya9@gmail.com>
2018-01-09 03:16:47 +0530

    amazon que 3

--- choclate.java
+++ choclate.java
@@ -1,3 +1,9 @@
+/*
+Geek has an array Arr, where Arr[i] (1-based indexing) denotes the number of chocolates corresponding to each station. When he move from station i to station i+1 he get A[i] – A[i+1] chocolates for free, if this number is negative, he looses that many chocolates also.
+He can only move from station i to station i+1, if he has non-negative number of chocolates with him.
+Given the cost of one chocolate is Rs. P, help geek to find the minimum cost incurred in reaching last station from the first station (station 1).
+Note: Initially Geek has 0 chocolate.
+*/
 import java.util.*;
 
 class Choclate{
