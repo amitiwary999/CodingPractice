342cdce930be527b36d135ccf7576b6372e4469e
amitiwary999 <tiwarya9@gmail.com>
2018-01-21 16:15:35 +0530

    microsoft que 4

--- nearestdivisible.java
+++ nearestdivisible.java
@@ -1,3 +1,6 @@
+/*
+Given two integers n and m. The problem is to find the number closest to n and divisible by m. If there are more than one such number, then output the one having maximum absolute value. If n is completely divisible by m(not equal to 0), then output n only. Time complexity of O(1) is required.
+*/
 import java.util.*;
 
 class NearestDivisible{
