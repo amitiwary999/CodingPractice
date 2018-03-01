4cbfd6484c4a7cd99fd855f42082b6f8c79221b6
amitiwary999 <tiwarya9@gmail.com>
2018-03-02 00:12:22 +0530

    microsoft que 25

--- addbinary.java
+++ addbinary.java
@@ -8,10 +8,23 @@ class AddBinary{
   String str1 = sc.next();
   String str2 = sc.next();
   String ans = null;
+  int i;
+  String newString;
   int sum = 0, carryp = 0, sum1=0;
-  int length = str1.length()>str2.length()?str2.length():str1.length();
-  for(int i=0;i<length;i++){
-    sum = (str1.charAt(i) - '0') + (str2.charAt(i) - '0');
+  int length1= str1.length();
+  int length2= str2.length();
+  int length, lengthnew; 
+  if(length1>length2){
+   length = length2;
+   lengthnew = length1;
+   newString = str1; 
+  }else{
+   length = length1;
+   lengthnew = length2;
+   newString = str2;
+  }
+  for(i=1;i<=length;i++){
+    sum = (str1.charAt(length1-i) - '0') + (str2.charAt(length2-i) - '0');
     if(sum == 2){
      sum = carryp;
      carryp = 1;
@@ -32,6 +45,31 @@ class AddBinary{
      ans = (Integer.toString(sum1));
     }
   }
+  for(int j=i;j<=lengthnew;j++){
+   sum = (newString.charAt(lengthnew-j) - '0') + carryp;
+   if(sum == 2){
+     sum = carryp;
+     carryp = 1;
+     if(ans!=null)
+      ans = (Integer.toString(sum))+ans;
+     else
+      ans = (Integer.toString(sum));
+    }else{
+     sum1 = (sum+carryp)%2;
+     if(sum1 == 0 && sum>0){
+      carryp = 1;
+     }else{
+      carryp=0;
+     }
+    if(ans!=null)
+     ans = (Integer.toString(sum1))+ans;
+    else
+     ans = (Integer.toString(sum1));  
+    }
+  }
+ if(carryp>0){
+  ans = (Integer.toString(carryp))+ans;
+ }
   System.out.println(ans);
  }
 }
