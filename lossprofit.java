af3401c49d1fc6c87fe379b90a4d09ef18340719
amitiwary999 <tiwarya9@gmail.com>
2018-03-06 23:44:02 +0530

    codechef march 2

--- lossprofit.java
+++ lossprofit.java
@@ -4,19 +4,23 @@ class LossProfit{
 
  public static void main(String[] args){
   Scanner sc = new Scanner(System.in);
+  int t = sc.nextInt();
+  for(int j=0;j<t;j++){
   int n = sc.nextInt();
   float ans = 0f;
   for(int i=0;i<n;i++){
    int price = sc.nextInt();
    int quantity = sc.nextInt();
    int dis = sc.nextInt();
-   int mul = 0;
+   float mul = 0f;
+   float disn = 0f;
    if(dis > 0){
-     mul = dis*quantity*price;
-     ans = (mul/1000f)+ans;
+     disn = dis/100f;
+     mul = disn*(float)price;
+     ans = (((float)price - (float)(price+mul)*(1-disn))*quantity)+ans;
    }
-   System.out.println(ans);
   }
  System.out.println(ans);
+}
  }
 }
