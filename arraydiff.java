9e411fd445d5b4bf8d84b60902259896735e87aa
amitiwary999 <tiwarya9@gmail.com>
2018-03-09 23:40:11 +0530

    updated

--- arraydiff.java
+++ arraydiff.java
@@ -15,16 +15,18 @@ class ArrayDiff{
   minArray[0] = arr[0];
   for(int j=1;j<size; j++){
    minArray[j] = arr[j] < minArray[j-1] ? arr[j] : minArray[j-1];
+   System.out.println(minArray[j]);
   }
 
   maxArray[size-1] = arr[size-1];
   for(int k=size-2;k>=0;k--){
-   maxArray[k] = arr[k] < maxArray[k+1] ? arr[k] : maxArray[k+1];
+   maxArray[k] = arr[k] > maxArray[k+1] ? arr[k] : maxArray[k+1];
+   System.out.println(maxArray[k]);
   } 
 
  int j=0,k=0,diff=0;
  while(j<size && k<size){
-   if(minArray[j] < maxArray[k]){
+   if(minArray[j] <= maxArray[k]){
      diff = (j-k) < diff ? diff : (j-k);
      j++;
    }else{
