74e8363e74082ae60a6103f2e864c635b0345365
amitiwary999 <tiwarya9@gmail.com>
2018-03-14 23:23:51 +0530

    que added

--- arraywave.java
+++ arraywave.java
@@ -1,3 +1,7 @@
+/*
+Given an array of integers, sort the array into a wave like array and return it. 
+In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
+*/
 import java.util.*;
 
 class ArrayWave{
