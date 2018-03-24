72bd17cdcf5bc8528860e97fd28a4b7262139706
amitiwary999 <tiwarya9@gmail.com>
2018-03-25 00:10:52 +0530

    amazon que 28(que added)

--- consecutiveones.java
+++ consecutiveones.java
@@ -1,3 +1,32 @@
+/*
+Given a binary array and an integer m, find the maximum number of consecutive 1's produced by flipping 0's. You can flip maximum m zeroes.
+Input:
+The first line contains an integer 'T' denoting the total number of test cases. In each test cases, the first line contains an integer 'N' denoting the size of array. The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.  Third line consists of an integer m that is maximum number of flips allowed.
+
+Output:
+Output the maximum numbers of consecutive 1's. (Indices begin from 0).
+
+Constraints:
+1<=T<=31
+1<=N<=100
+1<=m<=N
+
+Example:
+Input:
+1
+11
+1 0 0 1 1 0 1 0 1 1 1
+2
+Output:
+8
+Explaination: 
+Input:   arr[] = {1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1}
+         m = 2
+Output:  8
+We are allowed to flip maximum 2 zeroes. If we flip
+arr[5] and arr[7], we get 8 consecutive 1's which is
+maximum possible under given constraints
+*/
 import java.util.*;
 
 class ConsecutiveOne{
