9817968b377e643adcffcc5d348d7604d021c4e3
amitiwary999 <tiwarya9@gmail.com>
2018-03-26 23:12:12 +0530

    amazon que 29

--- midarraysum.java
+++ midarraysum.java
@@ -6,18 +6,19 @@ class MidArraySum{
   Scanner sc = new Scanner(System.in);
   System.out.println("Enter size of array");
   int size = sc.nextInt();
+  int i=0, j=0, sum=0;
   int array1[] = new int[size];
   int array2[] = new int[size];
   System.out.println("Enter array 1");
-  for(int i=0;i<size;i++){
+  for(int k=0;k<size;k++){
     array1[i] = sc.nextInt();
   }
   System.out.println("Enter array 2");
-  for(int i=0;i<size;i++){
+  for(int k=0;k<size;k++){
    array2[i] = sc.nextInt();
   }
-  while(i+j <= n){
-    if(i+j == n-1 || i+j == n){
+  while(i+j <= size){
+    if(i+j == size-1 || i+j == size){
       sum = sum+(array1[i] < array2[j] ? array1[i] : array2[j]);
     }
    if(array1[i] <= array2[j]){
