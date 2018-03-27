fb4f32eff1d13b5aec7c9433e828bd2d94ced6f1
amitiwary999 <tiwarya9@gmail.com>
2018-03-27 22:47:58 +0530

    amazon que 30

--- nextsmallpalin.java
+++ nextsmallpalin.java
@@ -3,16 +3,17 @@ import java.util.*;
 class NextSmallPalin{
 
  public static void main(String[] args){
-  Scanner sc = new Scanner(Sytem.in);
+  Scanner sc = new Scanner(System.in);
   System.out.println("Enter the size of array");
   int size = sc.nextInt();
-  int array = new int[size];
+  int array[] = new int[size];
   System.out.println("Enter array");
   int flag = size%2 == 0? 1:0;
-  for(int i=0;i<size;i++){
+  int i=0;
+  for(i=0;i<size;i++){
     array[i] = sc.nextInt();
   }
-  for(int i=0;i<size/2-1;i++){
+  for(i=0;i<size/2-1;i++){
    array[(size-1)-i] = array[i]; 
   }
   if(array[i] > array[(size-1)-i]){
@@ -20,7 +21,7 @@ class NextSmallPalin{
   }else{
    array[(size-1)-i] = array[i];
   }
-  for(int i=0;i<size;i++){
+  for(i=0;i<size;i++){
    System.out.println(array[i]+" ");
   }
  }
