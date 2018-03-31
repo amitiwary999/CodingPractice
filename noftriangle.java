c39a65f7c0b229adee17bbae8b7f53b2ee3e2399
amitiwary999 <tiwarya9@gmail.com>
2018-03-31 20:45:15 +0530

    amazo que 34

---
+++ noftriangle.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class NofTriangle{
+ public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter size of array");
+  int size = sc.nextInt();
+  int array[] = new int[size];
+  int min = 0, temp, flag, ans = 0;
+  System.out.println("Enter array element");
+  for(int i=0;i<size;i++){
+   array[i] = sc.nextInt();
+  }
+  for(int i=0;i<size-1;i++){
+   min = array[i];
+   for(int j=i+1; j<size;j++){
+    if(array[j] < min){
+     min = array[j];
+     flag = j;
+   }
+  }
+  temp = array[flag];
+  array[flag] = array[i];
+  array[i] = temp; 
+ }
+ for(int i=0;i<size-2;i++){
+  for(int j=i+1;j<size-1;j++){
+   for(int k=j+1; k<size;k++){
+    if(array[i]+array[j] >array[k] && array[i]+array[k] >array[j] && array[k]+array[j] >array[i]){
+     ans++;
+    } 
+   }
+  }
+ } 
+ System.out.println(ans);   
+ }
+}
