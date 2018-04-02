38d6fbb570fccd97635516ea94b7ca8b709ac31d
amitiwary999 <tiwarya9@gmail.com>
2018-04-02 20:39:43 +0530

    que added

--- nextsmallpalin.java
+++ nextsmallpalin.java
@@ -1,3 +1,21 @@
+/*
+Given a number, find the next smallest palindrome.
+Input :
+ The first line is number of test cases, second is size of array, third line contains digits of number with spaces in between.The input is assumed to be an array. Every entry in array represents a digit in input number. Let the array be ‘num[ ]’ and size of array be ’n’. Size of the number can be upto 1000 digits.
+Output:
+ In each separate line print the digits of palindrome with spaces in between.
+Constraints: 
+1<=T<=100
+1<=n<=1000
+1<=a[i]<=9
+Example: 
+Input: 
+1
+11
+9 4 1 8 7 9 7 8 3 2 2
+Output: 
+9 4 1 8 8 0 8 8 1 4 9
+*/
 import java.util.*;
 
 class NextSmallPalin{
