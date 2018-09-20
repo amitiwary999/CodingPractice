d15cb01fc38a416648da16c9b0db54d86efd550a
amitiwary999 <tiwarya9@gmail.com>
2018-09-20 22:53:12 +0530

    use meaningful variale name

--- hackerearth2prblm.java
+++ hackerearth2prblm.java
@@ -7,16 +7,16 @@ class HackerEarthPrblm2{
   Scanner sc = new Scanner(System.in);
   System.out.println("Enter number of testcases");
   int testCases = sc.nextInt();
-  int results[] = new int[26];
-  ArrayList<Integer> posorder = new ArrayList();
-  int posorder1[] = new int[26];
+  int letterCount[] = new int[26];
+  ArrayList<Integer> printLetterPresent = new ArrayList();
+  int printLetterNotPresent[] = new int[26];
   for(int i=0; i<testCases; i++){
 sc.nextLine();
     System.out.println("Enter the string");
     String str = sc.nextLine();
     int strLength = str.length();
     for(int j=0;j<strLength;j++){
-     results[str.charAt(j) - 'a']++;
+     letterCount[str.charAt(j) - 'a']++;
     }
     int flag = 0;
     for(int j=0;j<25;j++){
@@ -29,18 +29,18 @@ sc.nextLine();
        }
     }
       if(big >0){
-      int temp = results[j];
-      results[j] = results[pos];
-      results[pos] = temp;
-      posorder.add(pos);
-      posorder1[pos] = -1;
+      int temp = letterCount[j];
+      letterCount[j] = letterCount[pos];
+      letterCount[pos] = temp;
+      printLetterPresent.add(pos);
+      printLetterNotPresent[pos] = -1;
    }
    }
-  for(int j=0; j<posorder.size(); j++){
-     System.out.print((char)(posorder.get(j)+97)+" ");
+  for(int j=0; j<printLetterPresent.size(); j++){
+     System.out.print((char)(printLetterPresent.get(j)+97)+" ");
    }
  for(int j=0;j<26;j++){
-  if(posorder1[j] != -1){
+  if(printLetterNotPresent[j] != -1){
     System.out.print((char)(j+97)+" ");
   }
  }
