b656f847024c9ed1691d101592368e61cb808d7b
amitiwary999 <tiwarya9@gmail.com>
2019-05-07 02:13:34 +0530

    choclate distribution problem

---
+++ chocolate_destribution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class ChocolateDistribution{
+
+  public static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter number of packets");
+   int choclatePacket = sc.nextInt();
+   int[] choclates = new int[choclatePacket];
+   for(int i=0; i<choclatePacket;i++){
+     choclates[i] = sc.nextInt();
+   }
+
+   
+  }
+}
