552fea39ffca49584fa8e0f5cac7e9db4eb11c85
amitiwary999 <tiwarya9@gmail.com>
2019-05-11 21:40:54 +0530

    minimize works

---
+++ work_minimize.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class MaxWork{
+   public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter number of works");
+     int now = sc.nextInt();
+     System.out.println("Enter start and end time of work");
+     int startTime[] = new int[now];
+     int endTime[] = new int[now];
+     
+    for(int i=0; i<now; i++){
+      int s = sc.nextInt();
+      int e = sc.nextInt();
+   
+     startTime[i] = s;
+     endTime[i] = e;
+    }
+
+   for(int i=0; i<now; i++){
+
+   }
+   }
+}
