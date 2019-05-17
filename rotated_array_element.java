3162f03545820f4c9b8331562aa4c2e344fd8e92
amitiwary999 <tiwarya9@gmail.com>
2019-05-17 23:40:36 +0530

    working on rotated array

---
+++ rotated_array_element.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class RotatedArray{
+
+  public static void main(String[] args){
+    System.out.println("Enter number of element in array");
+    Scanner sc= new Scanner(System.in);
+ }
+}
