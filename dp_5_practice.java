f531129341b149043065f2d6a2ad8309f9b560c2
amitiwary999 <tiwarya9@gmail.com>
2019-05-31 22:29:24 +0530

    completed dp 5

--- dp_5_practice.java
+++ dp_5_practice.java
@@ -4,17 +4,22 @@ class DP_5_PRACTICE{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     String str1 = sc.next();
-    sc.next();
     String str2 = sc.next();
     char[] array1 = str1.toCharArray();
     char[] array2 = str2.toCharArray();
-    int[][] ans = new int[array1.lenght][array2.length]
-    for(int i=array1.length - 1; i<=0; i--){
-     for(int j=array2.length; j<=0; j--){
-       if(array1[i] == array2[j]){
-         ans[i][j] = 1;
+    int[][] ans = new int[array1.length+1][array2.length+1];
+    for(int i=0; i<=array1.length; i++){
+     for(int j=0; j<=array2.length; j++){
+       if(i==0 || j==0){
+         ans[i][j] = 0;
+       }else if(array1[i-1] == array2[j-1]){
+         ans[i][j] = ans[i-1][j-1]+1;
+       }else{
+         ans[i][j] = (ans[i-1][j] > ans[i][j-1])? ans[i-1][j] : ans[i][j-1];
        }
      }
     }
+
+   System.out.println(ans[array1.length][array2.length]);
   }
 }
