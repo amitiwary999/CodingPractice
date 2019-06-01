6f505a506d2fe42aca03a62e7bd6f7fc734e64aa
amitiwary999 <tiwarya9@gmail.com>
2019-06-01 23:55:12 +0530

    working on dp 6

---
+++ dp_6_practice.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class DP_6_PRACTICE{
+ public static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+   String str1 = sc.next();
+   String str2 = sc.next();
+
+   char[] str1Array = str1.toCharArray();
+   char[] str2Array = str2.toCharArray();
+
+   int[][] ans = new int[str1Array.length][str2Array.length];
+
+   for(int i=0; i<str1Array.length; i++){
+    for(int j=0; j<str2Array.length; j++){
+      if(str1Array[i] == str1Array[j]){
+        
+      }
+    }
+   }
+ }
+}
