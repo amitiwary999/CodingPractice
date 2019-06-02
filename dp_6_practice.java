d4acab138333a3f7f191ff8e960802999529a894
amitiwary999 <tiwarya9@gmail.com>
2019-06-02 19:21:57 +0530

    completed dp 6

--- dp_6_practice.java
+++ dp_6_practice.java
@@ -9,14 +9,27 @@ class DP_6_PRACTICE{
    char[] str1Array = str1.toCharArray();
    char[] str2Array = str2.toCharArray();
 
-   int[][] ans = new int[str1Array.length][str2Array.length];
+   int[][] ans = new int[str1Array.length+1][str2Array.length+1];
 
-   for(int i=0; i<str1Array.length; i++){
-    for(int j=0; j<str2Array.length; j++){
-      if(str1Array[i] == str1Array[j]){
-        
-      }
+   for(int i=1; i<=str1Array.length; i++){
+    for(int j=1; j<=str2Array.length; j++){
+      if(str1Array[i-1] == str2Array[j-1]){
+        ans[i][j] = ans[i-1][j-1];
+      }else{
+        ans[i][j] = 1+(min(ans[i][j-1],ans[i-1][j],ans[i-1][j-1]));
+     }
     }
+   }
+   System.out.println(ans[str1Array.length][str2Array.length]);
+ }
+
+public static int min(int a1, int a2, int a3){
+   if(a1 <= a2 && a1 <= a3){
+     return a1;
+   }else if(a2<=a1 && a2<=a3){
+     return a2;
+   }else{
+     return a3;
    }
  }
 }
