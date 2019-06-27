f7062626884192775acf9885b5b2a87f6a556912
amitiwary999 <tiwarya9@gmail.com>
2019-06-27 23:08:52 +0530

    trying some optimization

--- practice7.java
+++ practice7.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /*public class Solution {
 	public ArrayList<Integer> getRow(int a) {
 	    ArrayList<Integer> res = new ArrayList<Integer>();
@@ -15,7 +17,14 @@ Given an index k, return the kth row of the Pascal’s triangle.
 
 Pascal’s triangle : To generate A[C] in row R, sum up A’[C] and A’[C-1] from previous row R - 1.
 */
-public class Solution {
+class Solution {
+   public static void main(String[] args){
+     Solution sol = new Solution();
+     ArrayList<Integer> list = sol.getRow(70);
+     for(int i=0; i<list.size(); i++){
+       System.out.println(list.get(i));
+     }
+   }
     public ArrayList<Integer> getRow(int A) {
         ArrayList<Integer> ans = new ArrayList<Integer>();
         int prevTop = 1;
@@ -25,8 +34,10 @@ public class Solution {
                 ans.add(1);
             }else{
                 int temp = A-(i-1);
-                ans.add((temp*prevTop)/(i));
-                prevTop = (temp*prevTop)/i;
+                int flag = temp*ans.get(i-1)/i;
+               // System.out.println(temp*prevTop+"  "+(temp*prevTop)/i);
+                ans.add(flag);
+                prevTop = flag;
             }
         }
         return ans;
