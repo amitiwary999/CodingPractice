c1051e3c820082e9d5842e8a9a0fe97b19d09de8
amitiwary999 <tiwarya9@gmail.com>
2019-07-28 17:51:21 +0530

    practice 46

---
+++ practice46.java
@@ -0,0 +1,31 @@
+/*
+Given A, how many structurally unique BST’s (binary search trees) that store values 1...A?
+
+Example :
+
+Given A = 3, there are a total of 5 unique BST’s.
+
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+
+public class Solution {
+    public int numTrees(int n) {
+        int catalan[] = new int[n + 2]; 
+  
+        catalan[0] = 1; 
+        catalan[1] = 1; 
+  
+        for (int i = 2; i <= n; i++) { 
+            catalan[i] = 0; 
+            for (int j = 0; j < i; j++) { 
+                catalan[i] += catalan[j] * catalan[i - j - 1]; 
+            } 
+        } 
+        return catalan[n]; 
+    }
+}
