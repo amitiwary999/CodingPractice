f4ec2e56692b7335052c24f9b55cbfdad4571acb
amitiwary999 <tiwarya9@gmail.com>
2019-09-05 22:44:30 +0530

    complete practice leetcode 3

--- practiceleetcode3.java
+++ practiceleetcode3.java
@@ -1,3 +1,7 @@
+/*
+A string S of lowercase letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+*/
+
 class Solution {
     public List<Integer> partitionLabels(String S) {
         char[] charArray = S.toCharArray();
@@ -40,3 +44,23 @@ class Solution {
         return ans;
     }
 }
+/*
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
+        for (int i = 0; i < S.length(); ++i)
+            last[S.charAt(i) - 'a'] = i;
+        
+        int j = 0, anchor = 0;
+        List<Integer> ans = new ArrayList();
+        for (int i = 0; i < S.length(); ++i) {
+            j = Math.max(j, last[S.charAt(i) - 'a']);
+            if (i == j) {
+                ans.add(i - anchor + 1);
+                anchor = i + 1;
+            }
+        }
+        return ans;
+    }
+}
+*/
