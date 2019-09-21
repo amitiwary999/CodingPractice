e0719c18ed6e41db70576e6cc1328c4fa134de2a
amitiwary999 <tiwarya9@gmail.com>
2019-09-22 03:51:33 +0530

    practice leet code 23

---
+++ practiceleetcode23.java
@@ -0,0 +1,42 @@
+/*
+Given two strings s1, s2, find the lowest ASCII sum of deleted characters to make two strings equal.
+
+Example 1:
+Input: s1 = "sea", s2 = "eat"
+Output: 231
+Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
+Deleting "t" from "eat" adds 116 to the sum.
+At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
+Example 2:
+Input: s1 = "delete", s2 = "leet"
+Output: 403
+Explanation: Deleting "dee" from "delete" to turn the string into "let",
+adds 100[d]+101[e]+101[e] to the sum.  Deleting "e" from "leet" adds 101[e] to the sum.
+At the end, both strings are equal to "let", and the answer is 100+101+101+101 = 403.
+If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher.
+*/
+
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int row=s1.length();
+        int col=s2.length();
+        int[][] dp = new int[row+1][col+1];
+        dp[0][0]=0;
+        for(int i=1; i<=col; i++) {
+            dp[0][i]=dp[0][i-1] + (int) s2.charAt(i-1);
+        }
+        for(int i=1; i<=row; i++) {
+            dp[i][0]=dp[i-1][0] + (int) s1.charAt(i-1);
+        }
+        for(int i=1; i<=row; i++) {
+            for(int j=1; j<=col; j++) {
+                if(s1.charAt(i-1)==s2.charAt(j-1)) {
+                    dp[i][j]=dp[i-1][j-1];
+                } else {
+                    dp[i][j]= Math.min((dp[i][j-1] + (int) s2.charAt(j-1)), (dp[i-1][j] + (int) s1.charAt(i-1)));
+                }
+            }
+        }
+        return dp[row][col];
+    }
+}
